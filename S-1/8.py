b9ec8bce1d122acebd42ef09ccd7e472654d39dc
Lex2065-x <raulale.frt15@gmail.com>
2025-02-03 19:09:11 -0600

    area

--- S-1/8.py
+++ S-1/8.py
@@ -1,4 +1,4 @@
-pi = 3.1415
-r = float(input("ingrese el valor del radio: "))
-a = pi*(r**2)
+import math
+r = float(input("ingrese el valor del radio en cm: "))
+a = math.pi*(r**2)
 print("el valor dela area del circulo es: ", {a})
